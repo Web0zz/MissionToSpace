bd0b4f76df42be89ea3fd9b779310b12a2fdd56a
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-10-31 21:52:41 +0300

    NetworkHandler setup

---
+++ data/network/src/main/java/com/web0zz/network/util/InternetAvailablity.kt
@@ -0,0 +1,18 @@
+package com.web0zz.network.util
+
+import java.net.InetSocketAddress
+import java.net.Socket
+
+object InternetAvailability {
+
+    fun check() : Boolean =
+        try {
+            val socket = Socket()
+            socket.connect(InetSocketAddress("8.8.8.8",53))
+            socket.close()
+            true
+        } catch ( e: Exception){
+            e.printStackTrace()
+            false
+        }
+}
--- data/network/src/main/java/com/web0zz/network/util/NetworkHandler.kt
+++ data/network/src/main/java/com/web0zz/network/util/NetworkHandler.kt
@@ -2,33 +2,95 @@ package com.web0zz.network.util
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities
-import android.os.Build
+import android.net.NetworkRequest
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
+sealed class NetworkStatus {
+    object Available : NetworkStatus()
+    object UnAvailable : NetworkStatus()
+}
+
 class NetworkHandler @Inject constructor(
-    private val appContext: Context
-) {
-    fun checkNetworkStat(): Boolean {
-        val connectivityManager = appContext
-            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    appContext: Context,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+) : LiveData<NetworkStatus>() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+    private var connectivityManager: ConnectivityManager =
+        appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-            val network = connectivityManager.activeNetwork ?: return false
+    private val validNetworkConnections : ArrayList<Network> = ArrayList()
+    private lateinit var connectivityManagerCallback: ConnectivityManager.NetworkCallback
 
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                else -> false
-            }
-        } else {
-            @Suppress("DEPRECATION") val networkInfo =
-                connectivityManager.activeNetworkInfo ?: return false
-            @Suppress("DEPRECATION")
-            return networkInfo.isConnected
+    private fun announceStatus() {
+        when(validNetworkConnections.isNotEmpty()) {
+            true -> postValue(NetworkStatus.Available)
+            false -> postValue(NetworkStatus.UnAvailable)
         }
+    }
+
+    private fun getConnectivityManagerCallback() = object : ConnectivityManager.NetworkCallback() {
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+
+            val networkCapability = connectivityManager.getNetworkCapabilities(network)
+            val hasNetworkConnection =
+                networkCapability
+                    ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+
+            if (hasNetworkConnection){
+                determineInternetAccess(network)
+            }
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            validNetworkConnections.remove(network)
+            announceStatus()
+        }
+
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+
+            when (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+                true -> determineInternetAccess(network)
+                else -> validNetworkConnections.remove(network)
+            }
+            announceStatus()
+        }
+    }
+
+    private fun determineInternetAccess(network: Network) {
+        coroutineScope.launch{
+            if (InternetAvailability.check()){
+                withContext(Dispatchers.Main){
+                    validNetworkConnections.add(network)
+                    announceStatus()
+                }
+            }
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        connectivityManagerCallback = getConnectivityManagerCallback()
+        val networkRequest = NetworkRequest
+            .Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, connectivityManagerCallback)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(connectivityManagerCallback)
     }
 }
